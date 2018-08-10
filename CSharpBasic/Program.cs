ea3ee30119d146d9c2244541e43a0bd90f56fcee
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-10 15:51:17 -0400

    Working with Classes and Objects

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -8,10 +8,16 @@ namespace CSharpBasic
 {
     class Program
     {
+ 
         //Entry point of our program
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //instantiate an object of TestClass1
+
+            TestClass1 class1 = new TestClass1();
+            class1.TestCase1();
+            class1.TestCase2();
             Console.Read();
         }
     }
---
+++ CSharpBasic/TestClass1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    //all methods, variables and properties which we create inside a class are called  members of the class.
+    //When we create an object for the TestClass1 we will have access to all its members.
+
+    class TestClass1
+    {
+        public void TestCase1()
+        {
+            Console.WriteLine("Executing Test case1");
+            Console.Write("Hello World!");
+            Console.Read();
+        }
+
+        public void TestCase2()
+        {
+            Console.WriteLine("Executing Test case2");
+            Console.WriteLine("How are you");
+            Console.Read();
+
+
+        }
+ 
+    }
+}
