e2f77059294481b057cdb8272c1af7e4b8cd5299
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-12 01:21:03 -0400

    Working with multiple classes to access its members

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -16,8 +16,20 @@ namespace CSharpBasic
             //instantiate an object of TestClass1
 
             TestClass1 class1 = new TestClass1();
-            class1.TestCase1("Hello World!");
-            class1.TestCase2("How are you");
+            //class1.TestCase1("Hello World!");
+            //class1.TestCase2("How are you");
+
+            class1.SetValue(20);
+            class1.GetValue();
+            class1.SetValue(45);
+
+            TestClass1 class2 = new TestClass1();
+            class2.SetValue(40);
+            class2.GetValue();
+
+            //class1 called again has its own copy of the variable value which is a member
+            class1.GetValue();
+
 
             Console.Read();
         }
--- CSharpBasic/TestClass1.cs
+++ CSharpBasic/TestClass1.cs
@@ -11,6 +11,19 @@ namespace CSharpBasic
 
     class TestClass1
     {
+
+        private int i = 0;
+
+        public void SetValue(int value)
+        {
+            i = value;
+        }
+
+        public void GetValue()
+        {
+            Console.WriteLine("The value of i is : " + i);
+        }
+
         public void TestCase1(string result)
         {
             Console.WriteLine("Executing Test case1 " + result);
